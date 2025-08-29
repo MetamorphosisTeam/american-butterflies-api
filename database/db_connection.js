34ebc3a148519d6e186387874c7a9e4e701c67f3
gabriela-her <ari.ghb@gmail.com>
2025-08-29 21:23:13 +0200

    feat: add .env import to db_connection

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,10 +1,30 @@
-// config/db.js
-const { Sequelize } = require('sequelize');
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-// Configura la conexión a tu base de datos
-const sequelize = new Sequelize('american_butterflies', 'root', 'Pajaritos123', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+dotenv.config(); // Cargar variables de .env
 
-module.exports = sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+export default sequelize;
+
+
+
+//codigo anterior al cambio por el archivo .env
+
+// import { Sequelize } from "sequelize";
+
+// // Configura la conexión a tu base de datos
+// const sequelize = new Sequelize('american_butterflies', 'root', 'Pajaritos123', {
+//   host: 'localhost',
+//   dialect: 'mysql'
+// });
+
+// export default sequelize;
