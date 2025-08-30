867da15de8fad093ec7f282be9e0e07c220c0ed4
MichelleGel <michellegelves@gmail.com>
2025-08-30 16:08:38 +0200

    uploading environment for crontroller for methods

---
+++ controllers/ButterfliesController.js
@@ -0,0 +1,25 @@
+// para importar el model: import ButterfliesModel from "../models/ButterfliesModel.js"
+
+export const getAllButterflies = async (req, res) => {
+
+}
+
+export const getOneButterfly = async (req, res) => {
+
+}
+
+export const createButterfly = async (req, res) => {
+    try{
+
+    }catch(error){
+
+    }
+}
+
+export const updateButterfly = async (req, res) => {
+
+}
+
+export const deleteButterfly = async (req, res) => {
+
+}
