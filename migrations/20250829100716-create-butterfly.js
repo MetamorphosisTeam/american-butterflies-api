bf7bb0ea5617dbbbc67d104a365bb1b575c050f0
olgararo <olgararo1402@gmail.com>
2025-08-31 23:53:35 +0200

    fix: change db connection to config.json and adapt migration file to data model (#2)

--- migrations/20250829100716-create-butterfly.js
+++ migrations/20250829100716-create-butterfly.js
@@ -1,68 +1,72 @@
-'use strict';
+"use strict";
+
+import { DataTypes } from "sequelize";
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
- await queryInterface.createTable('butterflies', {
-  id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    order_name: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      defaultValue: 'Lepidoptera'
-    },
-    family: {
-      type: DataTypes.STRING(50),
-      allowNull: false
-    },
-    color: {
-      type: DataTypes.STRING(150),
-      allowNull: false
-    },
-    size: {
-      type: DataTypes.STRING(20),
-      allowNull: false
-    },
-    origin: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    location: {
-      type: DataTypes.STRING(200),
-      allowNull: false
-    },
-    habitat: {
-      type: DataTypes.TEXT
-    },
-    plants: {
-      type: DataTypes.TEXT
-    },
-    cycle: {
-      type: DataTypes.TEXT
-    },
-    img: {
-      type: DataTypes.STRING(500)
-    },
-    fenology: {
-      type: DataTypes.TEXT
-    }
-  
-  }, {
-
-    tableName: 'butterflies',
-    timestamps: true, 
-    underscored: true 
-  })
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("butterflies", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      order_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        defaultValue: "Lepidoptera",
+      },
+      family: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      color: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      origin: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+      habitat: {
+        type: DataTypes.TEXT,
+      },
+      plants: {
+        type: DataTypes.TEXT,
+      },
+      cycle: {
+        type: DataTypes.TEXT,
+      },
+      img: {
+        type: DataTypes.STRING(500),
+      },
+      fenology: {
+        type: DataTypes.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('butterflies');
-  }
+    await queryInterface.dropTable("butterflies");
+  },
 };
--- models/ButterfliesModel.js
+++ models/ButterfliesModel.js
@@ -1,63 +1,72 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/db_connection.js'); // Ajusta la ruta a tu archivo de conexión
+"use strict";
+const { Model, DataTypes } = require("sequelize");
 
-const Butterfly = sequelize.define('Butterfly', {
-  // Sequelize asume 'id' por defecto, pero lo definimos para claridad
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  order_name: {
-    type: DataTypes.STRING(50),
-    allowNull: false,
-    defaultValue: 'Lepidoptera'
-  },
-  family: {
-    type: DataTypes.STRING(50),
-    allowNull: false
-  },
-  color: {
-    type: DataTypes.STRING(150),
-    allowNull: false
-  },
-  size: {
-    type: DataTypes.STRING(20),
-    allowNull: false
-  },
-  origin: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  location: {
-    type: DataTypes.STRING(200),
-    allowNull: false
-  },
-  habitat: {
-    type: DataTypes.TEXT
-  },
-  plants: {
-    type: DataTypes.TEXT
-  },
-  cycle: {
-    type: DataTypes.TEXT
-  },
-  img: {
-    type: DataTypes.STRING(500)
-  },
-  fenology: {
-    type: DataTypes.TEXT
+module.exports = (sequelize) => {
+  class Butterfly extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index.js` file will call this method automatically.
+     */
+    static associate(models) {
+      // En el futuro, aquí puedes definir relaciones, por ejemplo:
+      // this.hasMany(models.OtherModel);
+    }
   }
-
-}, {
-  // Opciones adicionales del modelo
-  tableName: 'butterflies', // Le decimos a Sequelize que use este nombre exacto para la tabla
-  timestamps: true, // Habilita createdAt y updatedAt automáticamente
-  underscored: true // Mapea campos como `createdAt` a `created_at` en la BBDD
-});
-
-module.exports = Butterfly;
+  Butterfly.init(
+    {
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      order_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        defaultValue: "Lepidoptera",
+      },
+      family: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      color: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+      },
+      size: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      origin: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      location: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+      habitat: {
+        type: DataTypes.TEXT,
+      },
+      plants: {
+        type: DataTypes.TEXT,
+      },
+      cycle: {
+        type: DataTypes.TEXT,
+      },
+      img: {
+        type: DataTypes.STRING(500),
+      },
+      fenology: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Butterfly",
+      tableName: "butterflies",
+      timestamps: true,
+      underscored: true,
+    }
+  );
+  return Butterfly;
+};
