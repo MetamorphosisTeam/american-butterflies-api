3cad430cca701d6734f1ed3c75eb8360947782e6
MichelleGel <michellegelves@gmail.com>
2025-09-12 09:48:19 +0200

    fix/ added a custom sanitizer for optional fields validators and positive (if) on img validator

--- validators/butterflyValidators.js
+++ validators/butterflyValidators.js
@@ -20,6 +20,7 @@ export const ButterflyValidator = [
     // 2. Validación para 'order': opcional, ya que es de solo lectura en el form.
     check('order', 'El orden debe ser texto.')
         .optional({ checkFalsy: true })
+        .customSanitizer(value => value === "" ? null : value) // convierte "" en null
         .isIn(["Lepidoptera"])
         .withMessage('El orden debe ser "Lepidoptera".')
         .trim(),
@@ -29,28 +30,30 @@ export const ButterflyValidator = [
         .exists({ checkFalsy: true }) // No debe ser un string vacío ""
         .withMessage('La familia es obligatoria.')
         .bail()
-        .isIn(["Nymphalidae", "Papilionidae","Pieridae", "Lycaenidae", "Hesperiidae","Riodinidae"])
+        .isIn(["Nymphalidae", "Papilionidae", "Pieridae", "Lycaenidae", "Hesperiidae", "Riodinidae"])
         .withMessage('La familia seleccionada no es valida.')
         .trim(),
 
     // 4. Validación para 'img': opcional, pero si existe debe ser una URL de imagen válida.
     check('img')
         .optional({ checkFalsy: true })
+        .customSanitizer(value => value === "" ? null : value) // convierte "" en null
         .custom((value) => {
-            if (!/^https?:\/\/.*\.(jpg|jpeg|png|webp|gif)$/i.test(value)) {
+            if (value && !/^https?:\/\/.*\.(jpg|jpeg|png|webp|gif)$/i.test(value)) {
                 throw new Error('Debe ser una URL válida y terminar en .jpg, .jpeg, .png, .webp o .gif');
             }
             return true;
-        })
+        })//protegido para coincidir con el formulario(agregado por bug en post con el front)
         .trim(),
 
     // 5. Validación genérica para los campos de texto opcionales (textareas).
     ...['origin', 'location', 'color', 'size', 'fenology', 'cycle', 'habitat', 'plants'].map(field =>
-            check(field)
-                .optional({ checkFalsy: true })
-                .isString()
-                .isLength({ min: 4, max: 500 })
-                .withMessage(`El campo '${field}' debe tener entre 4 y 500 caracteres.`)
-                .trim()
-        ),
+        check(field)
+            .optional({ checkFalsy: true })
+            .customSanitizer(value => value === "" ? null : value) // convierte "" en null
+            .isString()
+            .isLength({ min: 4, max: 500 })
+            .withMessage(`El campo '${field}' debe tener entre 4 y 500 caracteres.`)
+            .trim()
+    ),
 ];
